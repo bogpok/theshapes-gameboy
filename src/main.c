18993e1463587d4a0994103f55b27ca843d5c2c2
Bogdan Pokrepin <bogpok@ya.ru>
2023-06-30 16:56:31 +0300

    next_state

--- src/main.c
+++ src/main.c
@@ -7,17 +7,32 @@
 #include "winmap.c"
 #include "tiles.c"
 
-UBYTE const DEBUG = 1;
 
-int8_t speed[2];
-BYTE jumping;
-
-uint8_t player_pos[2];
-
+const UBYTE DEBUG = 1;
 const uint8_t GRAVITY = 1;
 uint8_t ground_y = 108;
 
-void set_jumping(BYTE on){
+
+// === Player properties ===
+int8_t speed[2];
+BYTE jumping;
+uint8_t player_pos[2];
+
+uint8_t state = 0;
+uint8_t n_states = 2;
+UBYTE canChangeState = 1;
+
+void next_state(){
+    if (canChangeState){
+        state++;
+        if (state+1>n_states){
+            state = 0;
+        }
+        canChangeState = 0;
+    }  
+}
+
+void set_jumping(UBYTE on){
     jumping = on;
     
     
@@ -39,31 +54,45 @@ void jump()
              
 }
 
+/**
+ * Verify collision with map tileset
+ * When coverting from pixel x, y the offset (X: 1 tiles, Y: 2 tiles) is considered
+ * x, y: uint8_t coords [pixels]
+ * Return:
+ * UBYTE 0 - when there is a tile, which considered a blank, e.g. 0x00
+ * UBYTE 1 - otherwise
+*/
 UBYTE checkcollision(uint8_t x, uint8_t y){
-    uint8_t tile_i = x / 8;
-    uint8_t tile_j = y / 8;
-    char txy[10];
-    sprintf(txy, "X%d", tile_i);   
+    char charstub[10];
+    uint8_t tile_i = (x - 8) / 8;
+    uint8_t tile_j = (y - 16) / 8;
+
+    uint16_t tile_id = tile_i + bgWidth*tile_j;
+    UBYTE result = (bg[tile_id] == 0x00) || (bg[tile_id] == 0x25);
+    
+    // DEBUG WINDOW
+    sprintf(charstub, "X%d", tile_i);   
     draw_win_line(11, 0, "   ");  
-    draw_win_line(10, 0, txy);
-    sprintf(txy, "Y%d", tile_j); 
+    draw_win_line(10, 0, charstub);
+    sprintf(charstub, "Y%d", tile_j); 
     draw_win_line(16, 0, "   "); 
-    draw_win_line(15, 0, txy);
-
-    uint8_t tile_id = bgWidth*tile_i + tile_j;
-    char tile_idc[10];
-    sprintf(tile_idc, "%d", tile_id); 
+    draw_win_line(15, 0, charstub);    
+    
+    sprintf(charstub, "%d", tile_id); 
     draw_win_line(6, 2, "   ");     
-    draw_win_line(6, 2, tile_idc);
+    draw_win_line(6, 2, charstub);
 
-    UBYTE result = (bg[tile_id] == 0x00) || (bg[tile_id] == 0x24);
+    sprintf(charstub, "%d", bg[tile_id]); 
+    draw_win_line(10, 2, "   ");     
+    draw_win_line(10, 2, charstub);
+    
     if (result) {
         draw_win_line(6, 1, "NO ");
     } else {
         draw_win_line(6, 1, "YES");
     }   
     
-    return result;
+    return !result;
 }
 
 void move(uint8_t dt){    
@@ -125,13 +154,17 @@ void move(uint8_t dt){
     
     // Other controls    
     if (joypad() & J_A){
-        set_sprite_tile(0,1);               
+        //set_sprite_tile(0,1);               
     }
     if (joypad() & J_B){
-        set_sprite_tile(0,0);               
+        next_state();
+        
+        set_sprite_tile(0,state);               
+    } else {
+        canChangeState = 1;
     }
 
-   /*  switch(joypad()){            
+    /*  switch(joypad()){            
         case J_A:
             
             break;
@@ -139,19 +172,15 @@ void move(uint8_t dt){
             set_sprite_tile(0,0);
             break;            
     } */    
+
     int8_t dx = speed[0]*dt;
     if (!checkcollision(player_pos[0]+=dx, player_pos[1]+=speed[1]*dt)){
         player_pos[0]+=dx;
         player_pos[1]+=speed[1]*dt;
     }
-    
-
-
-    
-    
-
-    
     move_sprite(0, player_pos[0], player_pos[1]);
+
+    // === HANDLE BACKGROUND ===
     // dx, dy
     //scroll_bkg(dx, 0);
     //move_bkg(player_pos[0], player_pos[1]);
